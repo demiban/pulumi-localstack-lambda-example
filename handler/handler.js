1bebaa43c81a181eb7d2d38dcb15c36c143465b3
Demiban Diaz Torres <ing.diaz.6234@gmail.com>
2018-12-12 00:27:09 -0500

    Updated the body message!

--- handler/handler.js
+++ handler/handler.js
@@ -5,7 +5,7 @@ const handler = (payload, context, callback) => {
     callback(null, {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'Pepe approves this Demo!'
+            message: 'Hello from Lambda!'
         }),
         headers: {
             'X-Custom-Header': 'ASDF'
